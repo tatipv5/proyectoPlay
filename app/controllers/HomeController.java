88572eebe60fb6f99f79d18cfc9c836f2b0b5757
Luis Zárate <luis.zarate@solvosoft.com>
2016-05-27 17:23:40 -0600

    Maria: modificar y eliminar estudiante(contiene errores, muestra error en crear pregunta, del crud)

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,11 +1,13 @@
 package controllers;
 
+import java.util.List;
 import javax.inject.Inject;
 import play.data.Form;
 import play.mvc.*;
 import static play.mvc.Results.ok;
 import models.Estudiant;
 import play.data.FormFactory;
+import static play.mvc.Results.ok;
 
 import views.html.*;
 
@@ -30,25 +32,63 @@ public class HomeController extends Controller {
     //hola
     //get donde se crea la vEstudiante
     public Result crearEstudiantesGet() {
-        Form<Estudiant> pregForm = formFactory.form(Estudiant.class);
+        Form<Estudiant> EstuForm  = formFactory.form(Estudiant.class);
         return ok(crearEstudiant.render("Matricular Estudiante",
-                pregForm,
+               EstuForm,
                 routes.HomeController.crearEstudiantesPost()));
     }
-    
+    7
     //set, donde creo el post para el localhost nos salga en el servidor
      public Result crearEstudiantesPost() {
-        Form<Estudiant> pregForm = formFactory.form(Estudiant.class).bindFromRequest();
-        if (pregForm.hasErrors()) {
+        Form<Estudiant> EstuForm = formFactory.form(Estudiant.class).bindFromRequest();
+        if (EstuForm.hasErrors()) {
             return badRequest(crearEstudiant.render("Encontramos errores",
-                    pregForm, routes.HomeController.index()));
+                    EstuForm, routes.HomeController.index()));
         } else {
-            Estudiant preg = pregForm.get();
-            preg.save();
-            pregForm = formFactory.form(Estudiant.class);
+            Estudiant estu = EstuForm.get();
+            estu.save();
+            EstuForm = formFactory.form(Estudiant.class);
         }
-        return ok(crearEstudiant.render("La matricula ha sido creada correctamente", pregForm,
+        return ok(crearEstudiant.render("La matricula ha sido creada correctamente", EstuForm ,
                 routes.HomeController.crearEstudiantesPost()));
     }
+      public Result listaEstudiantes() {
+        List<Estudiant> estu = Estudiant.find.all();
+
+        return ok(crearEliminareditar.render("Listado de estudiantes", estu));
+    }
+     
+     public Result editarEstudianteGet(Long id) {
+        Estudiant instancia = Estudiant.find.byId(id);
+        Form<Estudiant> EstuForm = formFactory.form(Estudiant.class).fill(instancia);
+        return ok(crearEstudiant.render("Formulario de estudiante",
+                EstuForm, routes.HomeController.editarEstudiantePost(id)));
+    }
+
+    public Result editarEstudiantePost(Long id) {
+       Estudiant instancia = Estudiant.find.byId(id);
+        Form<Estudiant> EstuForm = formFactory.form(Estudiant.class
+        ).fill(instancia).bindFromRequest();
+
+        if (EstuForm .hasErrors()) {
+            return badRequest(crearEstudiant.render(
+                    "Encontramos errores", EstuForm ,
+                    routes.HomeController.editarEstudiantePost(id)
+            ));
+        }
+        
+      Estudiant estu = EstuForm.get();
+        instancia.id = estu.id;
+        instancia.nombre= estu.nombre;
+        instancia.direccion = estu.direccion;
+        instancia.telefono = estu.telefono;
+        instancia.save();
+        return redirect(routes.HomeController.crearEliminareditar());
+    }
+     public Result EliminarEstudiante(Long id) {
+        Estudiant instancia = Estudiant.find.byId(id);
+        instancia.delete();
+        return redirect(routes.HomeController.crearEliminareditar());
+    } 
 
 }
