6a2467de49a869e72a45dd146250b45c1bdc16fd
Bryan Blanco <bryansblanco@gmail.com>
2016-06-24 11:32:22 -0600

    ArrayCurso. Bryam

--- app/models/ArrayCurso.java
+++ app/models/ArrayCurso.java
@@ -5,10 +5,25 @@
  */
 package models;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author usuario
  */
 public class ArrayCurso {
+    
+    private ArrayList<Estudiant> listaEstudiante;
+ 
+      public ArrayCurso()
+      {
+         listaEstudiante=new ArrayList<Estudiant>();
+      }
+  
+    public void insertarCurso(Estudiant estudiante){  
+    listaEstudiante.add(estudiante);
+    JOptionPane.showMessageDialog(null,"Estudiante insertado a la lista");
+    }
     
 }
