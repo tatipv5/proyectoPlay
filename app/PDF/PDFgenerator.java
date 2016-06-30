92707509f9530ed703f763b1e40cea87dac8963d
tatipv5 <tatipv5@gmail.com>
2016-06-30 15:49:05 -0600

    Tati: se caen en el controller de grupo 113

--- app/PDF/PDFgenerator.java
+++ app/PDF/PDFgenerator.java
@@ -35,7 +35,7 @@ import java.util.List;
  *
  * @author usuario
  */
-<<<<<<< HEAD
+//<<<<<<< HEAD
 public class PDFgenerator {
 //    
 //    private List<String> defaultFonts = null;
@@ -133,104 +133,3 @@ public class PDFgenerator {
 //	}
 
 }//Fin de la clase.
-    
-=======
-//public class PDFgenerator {
-//    
-//    private List<String> defaultFonts = null;
-//
-//	public PDFgenerator() {
-//
-//	}
-//
-//	public void loadTemporaryFonts(List<String> fontsToLoad) {
-//		defaultFonts = new ArrayList<String>();
-//		addTemporaryFonts(fontsToLoad);
-//	}
-//
-//	public void addTemporaryFonts(List<String> fontsToLoad) {
-//		if (defaultFonts == null)
-//			defaultFonts = new ArrayList<String>();
-//		for (String font : fontsToLoad) {
-//			try {
-//				InputStream fin = Play.application().resourceAsStream(font);
-//				final File tempFile = File.createTempFile("tmp_" + FilenameUtils.getBaseName(font), "." + FilenameUtils.getExtension(font));
-//				tempFile.deleteOnExit();
-//				FileOutputStream out = new FileOutputStream(tempFile);
-//				IOUtils.copy(fin, out);
-//				defaultFonts.add(tempFile.getAbsolutePath());
-//			} catch (Exception e) {
-//				Logger.error("Loading fonts", e);
-//			}
-//		}
-//	}
-//
-//	public void loadLocalFonts(List<String> fontsToLoad) {
-//		defaultFonts = new ArrayList<String>();
-//		addLocalFonts(fontsToLoad);
-//	}
-//
-//	public void addLocalFonts(List<String> fontsToLoad) {
-//		if (defaultFonts == null)
-//			defaultFonts = new ArrayList<String>();
-//		for (String font : fontsToLoad)
-//			defaultFonts.add(font);
-//	}
-//
-//	public Result ok(Html html, String documentBaseURL) {
-//		byte[] pdf = toBytes(html.body(), documentBaseURL);
-//		return Results.ok(pdf).as("application/pdf");
-//	}
-//
-//	public Result ok(Html html, String documentBaseURL, List<String> fonts) {
-//		byte[] pdf = toBytes(html.body(), documentBaseURL, fonts);
-//		return Results.ok(pdf).as("application/pdf");
-//	}
-//
-//	public byte[] toBytes(Html html, String documentBaseURL) {
-//		byte[] pdf = toBytes(html.body(), documentBaseURL);
-//		return pdf;
-//	}
-//
-//	public byte[] toBytes(Html html, String documentBaseURL, List<String> fonts) {
-//		byte[] pdf = toBytes(html.body(), documentBaseURL, fonts);
-//		return pdf;
-//	}
-//
-//	public byte[] toBytes(String string, String documentBaseURL) {
-//		return toBytes(string, documentBaseURL, defaultFonts);
-//	}
-//
-//	public byte[] toBytes(String string, String documentBaseURL, List<String> fonts) {
-//		ByteArrayOutputStream os = new ByteArrayOutputStream();
-//		toStream(string, os, documentBaseURL, fonts);
-//		return os.toByteArray();
-//	}
-//
-//	public void toStream(String string, OutputStream os, String documentBaseURL) {
-//		toStream(string, os, documentBaseURL, defaultFonts);
-//	}
-//
-//	public void toStream(String string, OutputStream os, String documentBaseURL, List<String> fonts) {
-//		try {
-//			InputStream input = new ByteArrayInputStream(string.getBytes("UTF-8"));
-//			ITextRenderer renderer = new ITextRenderer();
-//			if (fonts != null)
-//				for (String font : fonts) {
-//					renderer.getFontResolver().addFont(font, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-//				}
-//			PdfUserAgent myUserAgent = new PdfUserAgent(renderer.getOutputDevice());
-//			myUserAgent.setSharedContext(renderer.getSharedContext());
-//			renderer.getSharedContext().setUserAgentCallback(myUserAgent);
-//			Document document = new HtmlDocumentBuilder().parse(input);
-//			renderer.setDocument(document, documentBaseURL);
-//			renderer.layout();
-//			renderer.createPDF(os);
-//		} catch (Exception e) {
-//			Logger.error("Error creating document from template", e);
-//		}
-//	}
-//
-//}//Fin de la clase.
-//    
->>>>>>> origin/master
