f425e9a63574d3715ec7e4d19ca45dc45ae80a30
che1416 <un_ga@hotmail.com>
2016-06-30 14:46:32 -0600

    Merge origin/master
    Conflicts:
    	app/controllers/ControllerEstudiant.java
    che
    che push
    chepull
    chepush
    Bryan push
    push mio

--- app/PDF/PDFuser.java
+++ app/PDF/PDFuser.java
@@ -26,93 +26,93 @@
  *
  * @author usuario
  */
-public class PDFuser  extends ITextUserAgent {
-
-	public PdfUserAgent(ITextOutputDevice outputDevice) {
-		super(outputDevice);
-	}
-
-	@Override
-	public ImageResource getImageResource(String uri) {
-		Option<InputStream> option = Play.current().resourceAsStream(uri);
-		if (option.isDefined()) {
-			InputStream stream = option.get();
-			try {
-				Image image = Image.getInstance(getData(stream));
-				scaleToOutputResolution(image);
-				return new ImageResource(uri, new ITextFSImage(image));
-			} catch (Exception e) {
-				Logger.error("fetching image " + uri, e);
-				throw new RuntimeException(e);
-			}
-		} else {
-			return super.getImageResource(uri);
-		}
-	}
-
-	@Override
-	public CSSResource getCSSResource(String uri) {
-		Option<InputStream> option = Play.current().resourceAsStream(uri);
-		if (option.isDefined())
-			return new CSSResource(option.get());
-		try {
-			// uri is in fact a complete URL
-			new URL(uri).getPath();
-			return super.getCSSResource(uri);
-		} catch (MalformedURLException e) {
-			Logger.error("fetching css " + uri, e);
-			throw new RuntimeException(e);
-		}
-	}
-
-	@Override
-	public byte[] getBinaryResource(String uri) {
-		Option<InputStream> option = Play.current().resourceAsStream(uri);
-		if (option.isDefined()) {
-			InputStream stream = option.get();
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			try {
-				copy(stream, baos);
-			} catch (IOException e) {
-				Logger.error("fetching binary " + uri, e);
-				throw new RuntimeException(e);
-			}
-			return baos.toByteArray();
-		} else {
-			return super.getBinaryResource(uri);
-		}
-	}
-
-	@Override
-	public XMLResource getXMLResource(String uri) {
-		Option<InputStream> option = Play.current().resourceAsStream(uri);
-		if (option.isDefined()) {
-			return XMLResource.load(option.get());
-		} else {
-			return super.getXMLResource(uri);
-		}
-	}
-
-	private void scaleToOutputResolution(Image image) {
-		float factor = getSharedContext().getDotsPerPixel();
-		image.scaleAbsolute(image.getPlainWidth() * factor, image.getPlainHeight() * factor);
-	}
-
-	private byte[] getData(InputStream stream) throws IOException {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		copy(stream, baos);
-		return baos.toByteArray();
-	}
-
-	private void copy(InputStream stream, OutputStream os) throws IOException {
-		byte[] buffer = new byte[1024];
-		while (true) {
-			int len = stream.read(buffer);
-			os.write(buffer, 0, len);
-			if (len < buffer.length)
-				break;
-		}
-	}
-}//Fin de la clase
-    
-
+//public class PDFuser  extends ITextUserAgent {
+//
+//	public PdfUserAgent(ITextOutputDevice outputDevice) {
+//		super(outputDevice);
+//	}
+//
+//	@Override
+//	public ImageResource getImageResource(String uri) {
+//		Option<InputStream> option = Play.current().resourceAsStream(uri);
+//		if (option.isDefined()) {
+//			InputStream stream = option.get();
+//			try {
+//				Image image = Image.getInstance(getData(stream));
+//				scaleToOutputResolution(image);
+//				return new ImageResource(uri, new ITextFSImage(image));
+//			} catch (Exception e) {
+//				Logger.error("fetching image " + uri, e);
+//				throw new RuntimeException(e);
+//			}
+//		} else {
+//			return super.getImageResource(uri);
+//		}
+//	}
+//
+//	@Override
+//	public CSSResource getCSSResource(String uri) {
+//		Option<InputStream> option = Play.current().resourceAsStream(uri);
+//		if (option.isDefined())
+//			return new CSSResource(option.get());
+//		try {
+//			// uri is in fact a complete URL
+//			new URL(uri).getPath();
+//			return super.getCSSResource(uri);
+//		} catch (MalformedURLException e) {
+//			Logger.error("fetching css " + uri, e);
+//			throw new RuntimeException(e);
+//		}
+//	}
+//
+//	@Override
+//	public byte[] getBinaryResource(String uri) {
+//		Option<InputStream> option = Play.current().resourceAsStream(uri);
+//		if (option.isDefined()) {
+//			InputStream stream = option.get();
+//			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//			try {
+//				copy(stream, baos);
+//			} catch (IOException e) {
+//				Logger.error("fetching binary " + uri, e);
+//				throw new RuntimeException(e);
+//			}
+//			return baos.toByteArray();
+//		} else {
+//			return super.getBinaryResource(uri);
+//		}
+//	}
+//
+//	@Override
+//	public XMLResource getXMLResource(String uri) {
+//		Option<InputStream> option = Play.current().resourceAsStream(uri);
+//		if (option.isDefined()) {
+//			return XMLResource.load(option.get());
+//		} else {
+//			return super.getXMLResource(uri);
+//		}
+//	}
+//
+//	private void scaleToOutputResolution(Image image) {
+//		float factor = getSharedContext().getDotsPerPixel();
+//		image.scaleAbsolute(image.getPlainWidth() * factor, image.getPlainHeight() * factor);
+//	}
+//
+//	private byte[] getData(InputStream stream) throws IOException {
+//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//		copy(stream, baos);
+//		return baos.toByteArray();
+//	}
+//
+//	private void copy(InputStream stream, OutputStream os) throws IOException {
+//		byte[] buffer = new byte[1024];
+//		while (true) {
+//			int len = stream.read(buffer);
+//			os.write(buffer, 0, len);
+//			if (len < buffer.length)
+//				break;
+//		}
+//	}
+//}//Fin de la clase
+//    
+//
